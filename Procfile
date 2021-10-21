0eefc92f56a480123efbd7d0c3783686f44e8e19
irinaignatenok <specterhauz@gmail.com>
2021-10-21 13:33:09 +0300

    new updates for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dresses.wsgi --log-file-
--- posts/tests.py
+++ posts/tests.py
@@ -18,7 +18,7 @@ class HomePageViewTest(TestCase):
         Post.objects.create(text='this is another test')
 
     def test_view_url_exists_at_proper_location(self):
-        resp=self.client.get('/')
+        resp = self.client.get('/')
         self.asserEqual(resp.status_code, 200)
 
     def test_view_url_by_name(self):
